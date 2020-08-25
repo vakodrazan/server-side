be796a392ac9d223f91f655453b4818e01995a81
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-08-25 10:50:57 +0300

    Add some javascript to allow us to see what's been serve

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 console.log('Greetings from Node and index.js! 🙃');
+
+// We need Express to act as our application server
+const express = require('express');
+
+// Let's create a new App
+const app = express();
+
+// We want express to run on this port
+const port = 3000;
+
+app.get('/', (request, response) => {
+  console.log('Request: /');
+  response.sendFile(__dirname + '/index.html');
+});
+
+app.listen(port, () => {
+  console.log(`app is running here: http://localhost:${port}`);
+});
