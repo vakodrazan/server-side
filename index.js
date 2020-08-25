3a85ce97775e16bf40333f473d0a5c7b11a35179
Noeline Marie Jeanne <noeline.mar@onja.org>
2020-08-25 11:30:49 +0300

    Using nunjucks file

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const nunjucks = require('nunjucks');
+
 console.log('Greetings from Node and index.js! 🙃');
 
 // We need Express to act as our application server
@@ -6,12 +8,16 @@ const express = require('express');
 // Let's create a new App
 const app = express();
 
+nunjucks.configure({
+  autoescape: true,
+  express: app
+});
+
 // We want express to run on this port
 const port = 3000;
 
 app.get('/', (request, response) => {
-  console.log('Request: /');
-  response.sendFile(__dirname + '/index.html');
+  response.render('./views/index.html.njk');
 });
 
 app.listen(port, () => {
